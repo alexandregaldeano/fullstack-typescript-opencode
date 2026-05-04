cf031503e96f37a75ce1b611869a2e80b05bc473
Alexandre Saou <alexandre.galdeano@gmail.com>
2026-05-04 10:32:51 +0000

    ci: merge workflow files into single linters.yml
    Consolidate actionlint, backend_pre_commit, and frontend_pre-commit
    workflows into a single linters.yml with 4 jobs:
    - actionlint
    - commitlint
    - backend-lint (ESLint + TypeScript check)
    - frontend-lint (ESLint + TypeScript check)
    
    Also move commitlint and config to root package.json, remove
    frontend/commitlint.config.js, and reduce header-max-length to 72
    chars to prevent PR title truncation.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
-export default {
+module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 72],
     'body-max-line-length': [2, 'always', 100],
   },
 };
